7d8337a6d240d3e0ba6f64d5b40a709a3fbf30da
sangeethashankar03 <20097745@mydbs.ie>
2026-02-25 09:49:15 +0000

    Update Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,29 @@
 provider "aws" {
-  region = "eu-west-1"  # your AWS region
+  region = "eu-west-1"
 }
 
-
+# Create key pair for EC2
 resource "aws_key_pair" "deployer" {
-  key_name   = "cloud-user" 
-  public_key = file("~/.ssh/cloud-user.pub") 
+  key_name   = "cloud-user"
+  public_key = var.public_key
 }
 
-
+# Security group allowing SSH and HTTP
 resource "aws_security_group" "allow_ssh_http" {
   name        = "allow_ssh_http"
   description = "Allow SSH and HTTP traffic"
-
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -35,12 +32,13 @@ resource "aws_security_group" "allow_ssh_http" {
   }
 }
 
-
+# EC2 instance
 resource "aws_instance" "web" {
-  ami           = "ami-03446a3af42c5e74e"  
-  instance_type = "t3.micro"
-  key_name      = aws_key_pair.deployer.key_name
-  security_groups = [aws_security_group.allow_ssh_http.name]
+  ami                         = "ami-03446a3af42c5e74e"  # EU West 1 Ubuntu AMI
+  instance_type               = "t3.micro"
+  key_name                    = aws_key_pair.deployer.key_name
+  vpc_security_group_ids      = [aws_security_group.allow_ssh_http.id]
+  associate_public_ip_address = true
 
   tags = {
     Name = "automation-web"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,4 @@
-variable "region" {
-  default = "eu-west-1"
+variable "public_key" {
+  type        = string
+  description = "SSH public key for EC2 access"
 }
