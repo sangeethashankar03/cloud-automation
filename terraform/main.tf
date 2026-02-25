1d428bdb7d8517449bb188c072ef99c98186f694
sangeethashankar03 <20097745@mydbs.ie>
2026-02-25 12:00:04 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,25 +2,44 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-# Use existing key pair (already created in AWS)
-data "aws_key_pair" "existing" {
+########################################
+# Use EXISTING Key Pair
+########################################
+data "aws_key_pair" "existing_key" {
   key_name = "cloud-user"
 }
 
-# Use existing security group (already created in AWS)
+########################################
+# Use EXISTING Security Group
+########################################
 data "aws_security_group" "existing_sg" {
   name = "allow_ssh_http"
 }
 
-# EC2 instance
+########################################
+# Create ONLY ONE EC2 Instance
+########################################
 resource "aws_instance" "web" {
-  ami                         = "ami-03446a3af42c5e74e"
-  instance_type               = "t3.micro"
-  key_name                    = data.aws_key_pair.existing.key_name
-  vpc_security_group_ids      = [data.aws_security_group.existing_sg.id]
+  ami           = "ami-03446a3af42c5e74e"   # Ubuntu 24.04 eu-west-1
+  instance_type = "t3.micro"
+
+  key_name               = data.aws_key_pair.existing_key.key_name
+  vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
+
   associate_public_ip_address = true
 
   tags = {
     Name = "automation-web"
   }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+########################################
+# Output Public IP
+########################################
+output "instance_ip" {
+  value = aws_instance.web.public_ip
 }
