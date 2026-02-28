dff44e804c67708a88b799d1c757c2d225c2b5ff
sangeethashankar03 <20097745@mydbs.ie>
2026-02-28 04:34:58 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,20 +2,62 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+# Get latest Ubuntu 22.04 LTS AMI dynamically
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+# Reference existing key pair
 data "aws_key_pair" "existing_key" {
   key_name = "aws-key"
 }
 
-data "aws_security_group" "existing_sg" {
-  name = "allow_ssh_http"
+# Create Security Group automatically
+resource "aws_security_group" "web_sg" {
+  name        = "web-automation-sg"
+  description = "Allow SSH and HTTP access"
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # You can restrict to your IP for better security
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-automation-sg"
+  }
 }
 
+# EC2 Instance
 resource "aws_instance" "web" {
-  ami           = "ami-03446a3af42c5e74e" # Ubuntu 24.04 eu-west-1
-  instance_type = "t3.micro"
-
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
   key_name               = data.aws_key_pair.existing_key.key_name
-  vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   associate_public_ip_address = true
 
